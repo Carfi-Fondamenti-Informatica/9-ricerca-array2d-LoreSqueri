b65931b5e1fd5acd06db98fe26970bf9a324d222
LoreSqueri <91483152+LoreSqueri@users.noreply.github.com>
2021-12-09 23:39:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,21 +1,15 @@
-//
-// Created by squer on 08/12/2021.
-//
-
+#include <iostream>
 #include "lib.h"
-bool res (char list[10][20],char name[20],int &i){
-    for(i=0;i<10;i++){
-        if(list[i][0]==name[0]){
-            for(int j=1;j<20;j){
-                if(j==19){
-                    return true;
-                }
-                j++;
-            }
-        }
-
-
+using namespace std;
+int main() {
+    char list [10][20]={"luca","giovanni","alberto","davide","francesco","franco","willy","matteo","denise","rosa"};
+    char name [20];
+    int a=0;
+    cin>>name;
+    if (res(list,name,a)){
+        cout <<a;
+    }else{
+        cout << "non presente";
     }
-    return false;
-
+    return 0;
 }
