b3fac17abb3742097e13eaf79e18fbd6923f260a
LoreSqueri <91483152+LoreSqueri@users.noreply.github.com>
2021-12-10 08:54:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include "lib.h"
 using namespace std;
 int main() {
-    char list [10][20]={"luca","giovanni","alberto","davide","francesco","franco","willy","matteo","denise","rosa"};
+    char list [10][20];
     char name [20];
     int a=0;
-    
+    for(int i=0;i<10;i++){
+        cin>>list[i];
+    }
     cin>>name;
     if (res(list,name,a)){
         cout <<a;
