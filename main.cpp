966f746969ad004517e465813332006835aaa501
LoreSqueri <91483152+LoreSqueri@users.noreply.github.com>
2021-12-10 08:52:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "lib.h"
 using namespace std;
 int main() {
-    char list [10][20]={"luca","giovanni","alberto","davide","francesco","franco","willy","matteo","denise","rosa"};;
+    char list [10][20]={"luca","giovanni","alberto","davide","francesco","franco","willy","matteo","denise","rosa"};
     char name [20];
     int a=0;
     
